ea5efa398b6141545c5cc05cd64b6727ec8e0c32
marcelo3543@hotmail.com <marcelo3543@hotmail.com>
2018-05-29 16:52:16 -0300

    Nuevo Proyecto MVC

---
+++ GestionCursos/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="GestionCursos.MvcApplication" Language="C#" %>
---
+++ GestionCursos/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(GestionCursos.Startup))]
+namespace GestionCursos
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
